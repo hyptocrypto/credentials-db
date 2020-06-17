872767e0cad10c16f68b233355c091c2afd17b2e
Julian baumgartner <julianbaumgartner@Julians-iMac.local>
2020-06-17 08:51:32 -0400

    Added delete service functino

--- credentials.py
+++ credentials.py
@@ -136,6 +136,70 @@ def decrypt_creds():
         print('\n----- Incorrect Password ------')
         return
 
+## Function to delete a service in the database. 
+def delete_service():
+    db.connect()
+    # Prompt for service name, check for exit and checks if the service name is in the db. 
+    service_input = input('What service would you like to delete?: ').lower()
+    service_input = service_input.strip()
+    if service_input == 'exit':
+        return
+    try:
+        service = Credentials.get(Credentials.service == service_input)
+    except Credentials.DoesNotExist:
+        print('\n ------ Sorry, please enter a valid service ------')
+        db.close()
+        decrypt_creds()
+        return
+    # Encoding encrypted username and password to bytes   
+    encrypted_username = service.username
+    encrypted_password = service.password
+    encrypted_username = bytes(encrypted_username, 'utf-8')
+    encrypted_password = bytes(encrypted_password, 'utf-8')
+    
+    # Prompt for password used to encrypt username and password.
+    pass_given = input('Please enter password:  ')
+    password = pass_given.encode()
+    salt = b'Y\xa8B\x85\x8d\x95\xe1\xb9\x0e\x19\x11\x17\x03.\n\x9d'
+
+    kdf = PBKDF2HMAC(
+        algorithm = hashes.SHA256(),
+        length =32, 
+        salt = salt,
+        iterations = 100000,
+    backend = default_backend()
+    )
+
+    key = base64.urlsafe_b64encode(kdf.derive(password)) 
+    f = Fernet(key)
+
+    # Tries to decrypt with given password. If successful, password was correct. 
+    # If the password was incorrect the progrma terminates to deter spamming password atempts. 
+    try:
+        decrypted_username = f.decrypt(encrypted_username)
+        decrypted_password = f.decrypt(encrypted_password)
+        username = decrypted_username.decode()
+        password = decrypted_password.decode()
+        print(f'\n Service: {service_input} \n Username: {username} \n password: {password}\n' +
+             '- Successfully Deleted -')
+        db.close()
+    except:
+        print('\n\n ------ Incorect Password ------')
+        return
+    # Check to see if service exits in database, if so, service is deleted.   
+    try:
+        delete =Credentials.delete().where(Credentials.service == service_input)
+        delete.execute()
+    except Credentials.DoesNotExist:
+        print('\n ----- Sorry, please enter a valid service -----')
+        db.close()
+        delete_service()
+        return
+
+
+
+
+
 # This function lists the service name of all saved entries
 def show_services():    
     db.connect()
@@ -152,13 +216,14 @@ def show_services():
 # Main logic of program. Prompt user to choose to make an entry, a querry or list saved services. 
 # Call the correct function acoriding to user input. 
 if __name__ == '__main__':
-    choices = ['a','b','c','q','e']
+    choices = ['a','b','c','d','q','e']
     
     
     while True:
         answer = input('Welcome! \n\nWould you like to save an input? (A) \n' +
                    'Would you like you like to make a querry? (B) \n'+
                       'Would you like to see all saved services? (C) \n' + 
+                      'Would you like to delete a service? (D)\n' +
                        '---(Q or E to exit )--- \n')
             
         if answer.lower() not in choices:
@@ -168,14 +233,19 @@ if __name__ == '__main__':
         elif answer.lower() == 'a':
             encrypt_creds()
             db.close()
-            break
+            continue
         elif answer.lower() == 'b':
             decrypt_creds()
             db.close()
-            break
+            continue
         elif answer.lower() == 'c':
             show_services()
-            break
+            continue
+        elif answer.lower() == 'd':
+            show_services()
+            delete_service()
+            db.close()
+            continue
         elif answer[0].lower() == 'q' or 'e':
             break
        
